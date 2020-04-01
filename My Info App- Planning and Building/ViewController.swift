aa8bb500f3c1705679fe99002949dad9e6ddef93
Breanna Badalov <BreannaBadalov@gmail.com>
2020-03-31 22:44:22 -0400

    Moon App Basics.

---
+++ My Info App- Planning and Building/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Info App- Planning and Building
+//
+//  Created by admin on 3/30/20.
+//  Copyright © 2020 Breanna Badalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
